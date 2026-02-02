562edcb7e2c8408eddc55350d5577ce623c80ad9
Sakshi Dethe <sakshidethe07@gmail.com>
2026-02-02 22:26:25 +0530

    Add files via upload

---
+++ ImplementationAndDemonstrationOfDataTypes.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace DataTypes
+{
+    class ImplementationAndDemonstrationOfDataTypes
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sakshi";
+
+            int age = 18;
+
+            float height = 5.3f;
+
+            char grade = 'A';
+
+            bool isStudent = true;
+
+            Console.WriteLine("Name (string): " + name);
+            Console.WriteLine("Age (int): " + age);
+            Console.WriteLine("Height (float): " + height);
+            Console.WriteLine("Grade (char): " + grade);
+            Console.WriteLine("Is Student (bool): " + isStudent);
+            Console.WriteLine("\nEnd of the program.");
+
+            Console.ReadKey();
+        }
+    }
+}
