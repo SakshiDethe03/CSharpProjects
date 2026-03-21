5702673c7bde4908e37568c2bba248f23fa77fba
Sakshi Dethe <sakshidethe07@gmail.com>
2026-03-21 23:26:27 +0530

    add program to find the maximum of two numbers

---
+++ MaxOfTwoNum.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter First Number: ");
+        int a = Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("Enter Second Number: ");
+        int b = Convert.ToInt16(Console.ReadLine());
+
+        if (a > b)
+        {
+            Console.WriteLine("Maximum number is" + a);
+        } 
+        else if (b > a)
+        {
+            Console.WriteLine("Maximum number is " + b);
+        }
+        else
+        {
+            Console.WriteLine("Both numbers are equal.");
+        }
+    }
+}
