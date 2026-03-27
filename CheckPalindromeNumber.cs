2f709a5491bb165821714a152f3471f38e491c4f
Sakshi Dethe <sakshidethe07@gmail.com>
2026-03-27 19:01:59 +0530

    Add a program of checking the palindrome number

---
+++ CheckPalindromeNumber.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num, original, reverse = 0, remainder;
+
+        Console.Write("Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        original = num;
+
+        while (num != 0)
+        {
+            remainder = num % 10;
+            reverse = reverse * 10 + remainder;
+            num = num / 10;
+        }
+
+        if (original == reverse)
+        {
+            Console.WriteLine("Number is Palindrome");
+        }
+        else
+        {
+            Console.WriteLine("Number is Not Palindrome");
+        }
+    }
+}
