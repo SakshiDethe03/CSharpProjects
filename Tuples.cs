6050ceed5c3487d7f70ba0af6f146221a0e841f7
Sakshi Dethe <sakshidethe07@gmail.com>
2026-04-07 00:42:21 +0530

    add program demonstrating the tuples in C Sharp

---
+++ Tuples.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = (Name: "Sakshi", Age: 20);
+
+        Console.WriteLine("Name: " + person.Name);
+        Console.WriteLine("Age: " + person.Age);
+    }
+}
