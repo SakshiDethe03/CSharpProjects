9cd495d3825c9a74cb507a88a205ae26beccf7d4
Sakshi Dethe <sakshidethe07@gmail.com>
2026-04-07 00:43:21 +0530

    add program demonstrating string interpolation

---
+++ StringInterpolation.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Sakshi";
+        int age = 20;
+
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
