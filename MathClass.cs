47b9eb7142a8a2d8e94743f4b14a6108722216af
Sakshi Dethe <sakshidethe07@gmail.com>
2026-04-07 00:44:20 +0530

    Add program demonstrating math class

---
+++ MathClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Square Root: " + Math.Sqrt(16));
+        Console.WriteLine("Power: " + Math.Pow(2, 3));
+        Console.WriteLine("Maximum: " + Math.Max(10, 20));
+        Console.WriteLine("Minimum: " + Math.Min(10, 20));
+    }
+}
