0dde464f5481b305f8742e23a1fe9c7f78f7bb1b
Sakshi Dethe <sakshidethe07@gmail.com>
2026-04-09 18:08:15 +0530

    add a program to reverse a string

---
+++ ReverseAString.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "hello";
+        char[] arr = str.ToCharArray();
+        Array.Reverse(arr);
+
+        Console.WriteLine(new string(arr));
+    }
+}
