716ccbeb206d1e751ece008f7e47393f0f748906
Sakshi Dethe <sakshidethe07@gmail.com>
2026-04-11 23:44:59 +0530

    add a program of static methods

---
+++ StaticMethods.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Show()
+    {
+        Console.WriteLine("Static Method Called");
+    }
+
+    static void Main()
+    {
+        Show();
+    }
+}
