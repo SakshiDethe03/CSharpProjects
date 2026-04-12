c24b05b79071915c9c5e8582dcfdd75eea63a8d3
Sakshi Dethe <sakshidethe07@gmail.com>
2026-04-12 23:26:00 +0530

    add a program of exception handling

---
+++ ExceptionHandling.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int a = 10, b = 0;
+            int c = a / b;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Execution Completed");
+        }
+    }
+}
