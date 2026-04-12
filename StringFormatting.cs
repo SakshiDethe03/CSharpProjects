906998be3e76c37a2e6be113a9af42b30d3938ce
Sakshi Dethe <sakshidethe07@gmail.com>
2026-04-12 20:50:45 +0530

    add a program of using string formatting

---
+++ StringFormatting.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Sakshi";
+        int age = 20;
+
+        Console.WriteLine(string.Format("Name: {0}, Age: {1}", name, age));
+    }
+}
