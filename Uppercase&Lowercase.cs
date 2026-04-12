9540dfa2a342c5274a5f3cf2f0774be2e1d9714a
Sakshi Dethe <sakshidethe07@gmail.com>
2026-04-12 20:41:05 +0530

    add program converting the string to uppercase and lowercase

---
+++ Uppercase&Lowercase.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "Hello";
+
+        Console.WriteLine(str.ToUpper());
+        Console.WriteLine(str.ToLower());
+    }
+}
