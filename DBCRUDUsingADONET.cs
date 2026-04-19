02ade022e96f17be93f5e85090003f11a23adb13
Sakshi Dethe <sakshidethe07@gmail.com>
2026-04-19 15:24:33 +0530

    add a program to perform database CRUD operations using ADO.NET

---
+++ DBCRUDUsingADONET.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connStr = "your_connection_string";
+
+        using (SqlConnection con = new SqlConnection(connStr))
+        {
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO Students VALUES (1,'A')", con);
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
